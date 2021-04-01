d5589eba7c0811597c8ed33a8585c57335393ecf
rayhan <rayhanaulia@students.amikom.ac.id>
2021-04-01 18:42:41 +0700

    overloading

---
+++ Number.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number
+{
+    class Number
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            return number1 + number2 - 3;
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            return (number2 + (int)number3) / number1;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            return (number1 - number2) * (int)number3;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+
+            Console.WriteLine("Minimum #1: {0}", number.FindMinimum(3, 1));
+            Console.WriteLine("Minimum #1: {0}", number.FindMinimum(3, 2, 4));
+            Console.WriteLine(); // tambahkan baris kosong
+            Console.WriteLine("Maximum #1: {0}", number.FindMaximum(3, 1));
+            Console.WriteLine("Maximum #1: {0}", number.FindMaximum(3, 2, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
